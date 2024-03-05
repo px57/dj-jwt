426873207adb3188aa58f190ae6547fc3b7a9244
px57 <david@prepera.io>
2024-03-05 10:14:03 -0300

    feature: Add authentication functionality to Service class
    Why:
    - The service class needs to have an authentication method for generating JWT tokens for users.
    
    Changes:
    - Added import statements for datetime, timedelta, and jwt
    - Modified the __init__ method to call the superclass constructor
    - Added an authenticate method to handle user authentication and generate JWT tokens

--- service.py
+++ service.py
@@ -3,19 +3,30 @@ import mailchimp_transactional as MailchimpTransactional
 
 from kernel.interfaces.service import ServiceManager
 
+from datetime import datetime, timedelta
+
+import jwt
+
 class Service(ServiceManager):
+
     def __init__(self):
-        pass
+        super().__init__()
+        print('ChildClass')
 
-    def init(self):
-        """
-        The constructor method.
-        """
-
-
-    def send_mail(self, *args, **kwargs):
+    def authenticate(self, *args, **kwargs):
         """
         Send mail method.
         """
-        print (args)
+        user = kwargs.get('user')
+
+        print ('Authenticate')
         print (kwargs)
+        config = self.get_config()
+        access_payload = {
+            'user_id': user.id,
+            'exp': datetime.utcnow() + timedelta(minutes=config['JWT_ACCESS_TOKEN_EXPIRES']),
+            'iat': datetime.utcnow(),
+        }
+        access_token = jwt.encode(access_payload, config['JWT_SECRET'], algorithm=config['JWT_ALGORITHM'])
+
+        self.res.jwt_token = access_token
